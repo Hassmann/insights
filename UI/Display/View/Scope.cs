c2026627c6c1779ff547c2d98ea032b446913318
Andreas Hassmann <andreas@hassmann.com>
2022-12-28 00:57:42 +0100

    Scope

---
+++ UI/Display/View/Scope.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SLD.Insights.UI.View
+{
+	class Scope : UIElement
+	{
+		const int lineLength = 10;
+
+		protected override void ArrangeCore(Rect finalRect)
+		{
+			base.ArrangeCore(finalRect);
+		}
+
+		protected override Size MeasureCore(Size availableSize)
+		{
+			return new Size(lineLength, lineLength);
+		}
+
+		protected override void OnRender(DrawingContext dc)
+		{
+			base.OnRender(dc);
+
+			dc.DrawLine(new Pen(new SolidColorBrush(Colors.Black), 3), new Point(), new Point(lineLength, lineLength));
+		}
+	}
+}
