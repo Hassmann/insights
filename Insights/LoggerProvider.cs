5da4e9f05042f35235029d648fa6e4cdee0064cd
Hassmann <andreas@hassmann.com>
2023-10-15 17:51:35 +0200

    only last part of name in LoggerProvider category

--- Insights/LoggerProvider.cs
+++ Insights/LoggerProvider.cs
@@ -5,7 +5,7 @@ namespace SLD.Insights
 	public class LoggerProvider : ILoggerProvider
 	{
 		public ILogger CreateLogger(string categoryName)
-			=> new LoggerAdapter(new InsightsSource(categoryName));
+			=> new LoggerAdapter(new InsightsSource(categoryName.Split('.').Last()));
 
 		public void Dispose()
 		{ }
